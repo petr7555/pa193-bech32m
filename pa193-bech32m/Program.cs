a989e6322e121eb4b7419b2977c8a3983d214acd
Petr Janik <peta.janik@email.cz>
2022-02-20 17:28:05 +0100

    Initialize project

---
+++ pa193-bech32m/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pa193_bech32m
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
