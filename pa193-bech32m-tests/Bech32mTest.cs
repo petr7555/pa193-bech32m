202d42a7bc9ad771c8343c0e112e52112b7a1484
Petr Janik <peta.janik@email.cz>
2022-03-13 09:53:01 +0100

    Reformat code

--- pa193-bech32m-tests/Bech32mTest.cs
+++ pa193-bech32m-tests/Bech32mTest.cs
@@ -60,18 +60,22 @@ namespace pa193_bech32m_tests
 
 
         [TestCase("BC1QW508D6QEJXTDG4Y5R3ZARVARY0C5XW7KV8F3T4", "0014751e76e8199196d454941c45d1b3a323f1433bd6")]
-        [TestCase("tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7", "00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262")]
-        [TestCase("bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kt5nd6y", "5128751e76e8199196d454941c45d1b3a323f1433bd6751e76e8199196d454941c45d1b3a323f1433bd6")]
+        [TestCase("tb1qrp33g0q5c5txsp9arysrx4k6zdkfs4nce4xj0gdcccefvpysxf3q0sl5k7",
+            "00201863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262")]
+        [TestCase("bc1pw508d6qejxtdg4y5r3zarvary0c5xw7kw508d6qejxtdg4y5r3zarvary0c5xw7kt5nd6y",
+            "5128751e76e8199196d454941c45d1b3a323f1433bd6751e76e8199196d454941c45d1b3a323f1433bd6")]
         [TestCase("BC1SW50QGDZ25J", "6002751e")]
         [TestCase("bc1zw508d6qejxtdg4y5r3zarvaryvaxxpcs", "5210751e76e8199196d454941c45d1b3a323")]
-        [TestCase("tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy", "0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433")]
-        [TestCase("tb1pqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesf3hn0c", "5120000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433")]
-        [TestCase("bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0", "512079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798")]
+        [TestCase("tb1qqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesrxh6hy",
+            "0020000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433")]
+        [TestCase("tb1pqqqqp399et2xygdj5xreqhjjvcmzhxw4aywxecjdzew6hylgvsesf3hn0c",
+            "5120000000c4a5cad46221b2a187905e5266362b99d5e91c6ce24d165dab93e86433")]
+        [TestCase("bc1p0xlxvlhemja6c4dqv22uapctqupfhlxm9h8z3k2e72q4k9hcz7vqzk5jj0",
+            "512079be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798")]
         public void DecodesAndEncodesValidStrings(string input, string output)
         {
             Assert.Equals(Bech32m.Decode(input), output);
             Assert.Equals(Bech32m.Encode(output), input);
         }
-
     }
 }
