0c71e8a3cb6435ddec546542d5fe8910437ffa2c
Petr Janik <peta.janik@email.cz>
2022-03-15 23:14:10 +0100

    Improve CLI API proposal

--- pa193-bech32m/Program.cs
+++ pa193-bech32m/Program.cs
@@ -7,25 +7,29 @@ namespace pa193_bech32m
         static void Main(string[] args)
         {
             // CLI interface
+            // should be hard to misuse
             // if -o is not specified, stdout is used by default
+            // hrp is always passed as ASCII cli argument
             
             // Encode
             
             // cli-argument
             // usage: bech32m encode --format <format> <hrp> <data> [-o <outputfile>]
-            // eg. bech32m encode --format base64 YXZj Eu8=
-            // eg. bech32m encode --format hex 616263 12ef -o result
-            // eg. bech32m encode --format binary 011000010110001001100011 0001001011101111
+            // eg. bech32m encode --format base64 abc Eu8=
+            // eg. bech32m encode --format hex abc 12ef -o result
+            // eg. bech32m encode --format binary abc 0001001011101111
             
             // file
-            // usage: bech32m encode --format <format> --file <filename> [-o <outputfile>]
-            // input file contains hrp on the first line and data on the second line
+            // usage: bech32m encode --format <format> --file <filename> <hrp> [-o <outputfile>]
+            // input file contains only data
+            // in case of binary input, the file is binary (it does not contain ascii zeroes and ones)
 
             // stdin (default)
-            // usage: bech32m encode --format <format> [-o <outputfile>]
-            // waits for user input for hrp
-            // enter
+            // usage: bech32m encode --format <format> <hrp> [-o <outputfile>]
             // waits for user input for data
+            // should work with pipe
+            // https://stackoverflow.com/a/199534/9290771
+            // https://stackoverflow.com/a/1562452/9290771
 
             // Decode
             // TODO propose "decode" user interface
