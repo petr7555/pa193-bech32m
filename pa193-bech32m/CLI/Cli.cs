1c7db4fb91a8882726c6963cdd863beb3fb5747e
Petr Janik <peta.janik@email.cz>
2022-04-07 12:00:53 +0200

    Remove notes for encode CLI interface

--- pa193-bech32m/CLI/Cli.cs
+++ pa193-bech32m/CLI/Cli.cs
@@ -144,31 +144,6 @@ namespace pa193_bech32m.CLI
             PrintError($"unknown command '{arg}'");
             return ExitFailure;
 
-            // CLI interface
-            // should be hard to misuse
-            // if -o is not specified, stdout is used by default
-            // hrp is always passed as ASCII cli argument
-
-            // Encode
-
-            // cli-argument
-            // usage: bech32m encode --format <format> <hrp> <data> [-o <outputfile>]
-            // eg. bech32m encode --format base64 abc Eu8=
-            // eg. bech32m encode --format hex abc 12ef -o result
-            // eg. bech32m encode --format binary abc 0001001011101111
-
-            // file
-            // usage: bech32m encode --format <format> --file <filename> <hrp> [-o <outputfile>]
-            // input file contains only data
-            // in case of binary input, the file is binary (it does not contain ascii zeroes and ones)
-
-            // stdin (default)
-            // usage: bech32m encode --format <format> <hrp> [-o <outputfile>]
-            // waits for user input for data
-            // should work with pipe
-            // https://stackoverflow.com/a/199534/9290771
-            // https://stackoverflow.com/a/1562452/9290771
-
             // Decode
             // TODO propose "decode" user interface
             // In case of erroneous decoding suggest the closest valid input.
